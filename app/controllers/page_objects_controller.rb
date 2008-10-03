eeb519b2b93acd7c22fa83246fbd0c6e944757a0
Matthew Moore <matt@thrivesmart.com>
2008-10-03 10:07:08 -0700

    updated tsapp rails

--- app/controllers/page_objects_controller.rb
+++ app/controllers/page_objects_controller.rb
@@ -1,6 +1,7 @@
 class PageObjectsController < ApplicationController
   include PageObjectsControllerHelper
-  before_filter :find_page_object, :only => [ :show, :edit, :update, :destroy, :update_valid ]
+  before_filter :find_page_object, :only => [ :show, :edit, :update, :destroy ]
+  before_filter :prepare_page_object_for_clone, :only => [ :duplicate ]
   
   # GET /page_objects
   # GET /page_objects.xml
@@ -16,7 +17,7 @@ class PageObjectsController < ApplicationController
   # GET /page_objects/1
   # GET /page_objects/1.xml
   def show
-    set_html
+    render_to_page_object
   
     respond_to do |format|
       format.html # show.html.erb
@@ -28,7 +29,7 @@ class PageObjectsController < ApplicationController
   # GET /page_objects/new.xml
   def new
     @page_object = PageObject.new(params[:page_object])
-    set_html
+    render_to_page_object
 
     respond_to do |format|
       format.html # new.html.erb
@@ -38,7 +39,7 @@ class PageObjectsController < ApplicationController
 
   # GET /page_objects/1/edit
   def edit
-    set_html
+    render_to_page_object
     
     respond_to do |format|
       format.html # edit.html.erb
@@ -57,7 +58,24 @@ class PageObjectsController < ApplicationController
         format.html { redirect_to(@page_object) }
         format.xml  { render :xml => @page_object, :status => :created, :location => @page_object }
       else
-        set_html
+        render_to_page_object
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @page_object, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  # POST /page_objects/duplicate?source=:urn
+  # POST /page_objects/duplicate.xml?source=:urn
+  def duplicate
+
+    respond_to do |format|
+      if @page_object.save
+        flash[:notice] = 'Page Object was successfully created.'
+        format.html { redirect_to(@page_object) }
+        format.xml  { render :xml => @page_object, :status => :created, :location => @page_object }
+      else
+        render_to_page_object
         format.html { render :action => "new" }
         format.xml  { render :xml => @page_object, :status => :unprocessable_entity }
       end
@@ -74,7 +92,7 @@ class PageObjectsController < ApplicationController
         format.html { redirect_to(@page_object) }
         format.xml  { head :ok }
       else
-        set_html
+        render_to_page_object
         format.html { render :action => "edit" }
         format.xml  { render :xml => @page_object, :status => :unprocessable_entity }
       end
